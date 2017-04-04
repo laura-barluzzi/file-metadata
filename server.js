8c3e9a640e79100687d6bab18cff1a82a2e1d53f
Laura Barluzzi <laura@koalacoder.com>
2017-04-04 23:42:12 +0000

    show first page with buttons

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express')
 var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var path = require('path')
 
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'home.html')) // absolute path
+})
+
+app.post('/', function(req, res) {
+    res.end()
+})
+
+app.get('/get-file-size', function (req, res) {
+    res.send('file size is X')
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!')
 })
